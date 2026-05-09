035ae1a6ceed368fefd3b30774d4e554f1d8e7ef
Himvat Dev <himvat.dev@gmail.com>
2026-05-09 22:35:10 +0545

    Add GlobalSkins.php to load Vector skin
    Load the Vector skin and set it as the default skin.

---
+++ GlobalSkins.php
@@ -0,0 +1,12 @@
+<?php
+wfLoadSkins( [
+    'Vector',
+    
+] );
+// Choose one of these and add chosen code to bottom of LocalSettings.php file:
+//$wgDefaultSkin
+
+
+
+
+$wgDefaultSkin = 'vector-2022';
